a89b41ff50fec638c026c7b19865bd4a9a8a8d74
Sindre Bøyum <sindre.boyum@gmail.com>
2022-01-23 19:40:55 +0100

    use `git` instead of `gh` to clone repo

--- src/helpers/git.helpers.ts
+++ src/helpers/git.helpers.ts
@@ -1,18 +1,23 @@
 import * as exec from "@actions/exec";
 import * as github from "@actions/github";
 
-export async function checkoutMain(directoryName: string): Promise<void> {
+export async function checkoutRepo(directoryName: string): Promise<void> {
   const { owner, repo } = github.context.repo;
-  await exec.exec(`gh repo clone ${owner}/${repo} ${directoryName}`);
+  const gitHubUri = `git@github.com:${owner}/${repo}.git`;
+
+  await exec.exec(`git clone ${gitHubUri} ${directoryName}`);
+}
+
+export async function checkoutMain(directoryName: string): Promise<void> {
+  await checkoutRepo(directoryName);
 }
 
 export async function checkoutCurrentBranch(
   directoryName: string,
 ): Promise<void> {
-  const { owner, repo } = github.context.repo;
-  const currentBranch = github.context.ref.replace("refs/heads/", "");
+  await checkoutRepo(directoryName);
 
-  await exec.exec(`gh repo clone ${owner}/${repo} ${directoryName}`);
-  await exec.exec("cd current-branch");
+  const currentBranch = github.context.ref.replace("refs/heads/", "");
+  await exec.exec(`cd ${directoryName}`);
   await exec.exec(`git checkout ${currentBranch}`);
 }
